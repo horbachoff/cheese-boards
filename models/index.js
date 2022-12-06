f1ec970f6890061cb65efe85925fe23f8b522b35
horbachoff <mr.horbachov@gmail.com>
2022-12-06 15:43:20 -0500

    Create many to many relations for User Cheese and Board

--- models/index.js
+++ models/index.js
@@ -2,4 +2,10 @@ const { User } = require('./User');
 const { Board } = require('./Board');
 const { Cheese } = require('./Cheese');
 
+User.hasMany(Board)
+Board.belongsToMany(User)
+
+Cheese.belongsToMany(Board, {through: 'cheese_board'})
+Board.belongsToMany(Cheese, {through: 'cheese_board'})
+
 module.exports = { User, Board, Cheese };
